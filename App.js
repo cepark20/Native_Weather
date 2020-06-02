e1c46d1753ed3111a0bb1447c043d0919bd11d0f
Jane Park <cepark.dev@gmail.com>
2020-06-03 03:02:37 +0900

    Asking for Permissions

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import Loading from "./Loading";
 import * as Location from "expo-location";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 
 export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
   getLocation = async() =>{
-    const location = await Location.getCurrentPositionAsync();
-  }
+    try {
+      await Location.requestPermissionsAsync();
+      //const {coords} = await Location.getCurrentPositionAsync();
+      const {coords:{latitude, longitude}} = await Location.getCurrentPositionAsync();
+            
+      this.setState({isLoading : false});
+      //Send to API and get weather
+    } catch (error) {
+        Alert.alert("Location Error", "설정에서 위치 권한을 허용해주십시오.");
+    }  
+  };
   componentDidMount(){
     this.getLocation();
   }
   render(){
-    return <Loading />;
+    const {isLoading} = this.state;
+    return isLoading ? <Loading /> : null;
   }
 }
 
